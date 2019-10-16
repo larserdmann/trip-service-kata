21ccc4566f2ea291c6a741eb2bd75099c6519c30
Nick <nick.mueller@uni-greifswald.de>
2019-10-16 12:34:41 +0200

    Refactor #1

--- java/trip-service-kata/src/main/java/org/craftedsw/tripservicekata/trip/TripService.java
+++ java/trip-service-kata/src/main/java/org/craftedsw/tripservicekata/trip/TripService.java
@@ -9,39 +9,39 @@ import org.craftedsw.tripservicekata.user.UserSession;
 
 public class TripService {
 
-	private UserSession session = UserSession.getInstance();
-	private TripDAO dao;
+    private UserSession session = UserSession.getInstance();
+    private TripDAO dao;
 
-	public TripService(TripDAO dao) {
-		this.dao = dao;
-	}
+    public TripService(TripDAO dao) {
+        this.dao = dao;
+    }
 
-	public void setSession(final UserSession session) {
-		this.session = session;
-	}
+    public void setSession(final UserSession session) {
+        this.session = session;
+    }
 
-	public List<Trip> getTripsByUser(User user) throws UserNotLoggedInException {
-		List<Trip> tripList = new ArrayList<Trip>();
-		User loggedUser = session.getLoggedUser();
-		boolean isFriend = false;
-		if (loggedUser != null) {
-			for (User friend : user.getFriends()) {
-				if (friend.equals(loggedUser)) {
-					isFriend = true;
-					break;
-				}
-			}
-			if (isFriend) {
-				tripList = findTripsByUser(user);
-			}
-			return tripList;
-		} else {
-			throw new UserNotLoggedInException();
-		}
-	}
+    public List<Trip> getTripsByUser(User user) throws UserNotLoggedInException {
+        List<Trip> tripList = new ArrayList<Trip>();
+        User loggedUser = session.getLoggedUser();
+        boolean isFriend = false;
 
-	List<Trip> findTripsByUser(final User user) {
-		return dao.findTripsByUser2(user);
-	}
+        if (loggedUser == null)
+            throw new UserNotLoggedInException();
+
+        for (User friend : user.getFriends()) {
+            if (friend.equals(loggedUser)) {
+                isFriend = true;
+                break;
+            }
+        }
+        if (isFriend) {
+            tripList = findTripsByUser(user);
+        }
+        return tripList;
+    }
+
+    List<Trip> findTripsByUser(final User user) {
+        return dao.findTripsByUser2(user);
+    }
 
 }
